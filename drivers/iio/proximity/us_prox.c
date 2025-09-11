94c85fab2973c812a89b31c7f41c715ae0b4ebe6
Enceka <enceka@yeah.net>
2025-09-11 21:55:42 +0800

    drivers: iio: proximity: Revert to stock

--- drivers/iio/proximity/us_prox.c
+++ drivers/iio/proximity/us_prox.c
@@ -44,24 +44,24 @@
 #include <audio_ipi_queue.h>
 
 /*
- e xtern void iio_device*_unregister(struct iio_dev *indio_dev);
- extern void iio_device_free(struct iio_dev *dev);
- extern int __iio_device_register(struct iio_dev *indio_dev, struct module *this_mod);
- extern struct iio_dev *iio_device_alloc(struct device *parent, int sizeof_priv);
- */
+extern void iio_device_unregister(struct iio_dev *indio_dev);
+extern void iio_device_free(struct iio_dev *dev);
+extern int __iio_device_register(struct iio_dev *indio_dev, struct module *this_mod);
+extern struct iio_dev *iio_device_alloc(struct device *parent, int sizeof_priv);
+*/
 struct iio_buffer *iio_kefifo_allocate(void);
 void iio_kefifo_free(struct iio_buffer *r);
 struct iio_poll_func
 *iio_alloc_pollfunc(irqreturn_t (*h)(int irq, void *p),
-					irqreturn_t (*thread)(int irq, void *p),
-					int type,
-					struct iio_dev *indio_dev,
-					const char *fmt,
-					...);
+        irqreturn_t (*thread)(int irq, void *p),
+        int type,
+        struct iio_dev *indio_dev,
+        const char *fmt,
+        ...);
 int iio_trigger_buffer_setup(struct iio_dev *indio_dev,
-							 irqreturn_t (*h)(int irq, void *p),
-							 irqreturn_t (*thread)(int irq, void *p),
-							 const struct iio_buffer_setup_ops *setup_ops);
+        irqreturn_t (*h)(int irq, void *p),
+        irqreturn_t (*thread)(int irq, void *p),
+        const struct iio_buffer_setup_ops *setup_ops);
 void iio_trigger_buffer_cleanup(struct iio_dev *indio_dev);
 
 void iio_dealloc_pollfunc(struct iio_poll_func *pf);
@@ -77,19 +77,19 @@ struct mius_ipi_scp_to_host_message_header {
 	uint16_t data_size;
 };
 typedef
-struct mius_ipi_scp_to_host_message_header
-mius_ipi_scp_to_host_message_header_t;
+	struct mius_ipi_scp_to_host_message_header
+	mius_ipi_scp_to_host_message_header_t;
 
 #define MIUS_IPI_SCP_TO_AP_DATA_SIZE 40
 #define MIUS_DRAM_PAYLOAD_MAX_OFFSET ((uint16_t) 8)
 struct mius_ipi_scp_to_host_message {
 	mius_ipi_scp_to_host_message_header_t header;
 	uint8_t data[MIUS_IPI_SCP_TO_AP_DATA_SIZE -
-	sizeof(mius_ipi_scp_to_host_message_header_t)];
+			sizeof(mius_ipi_scp_to_host_message_header_t)];
 };
 typedef
-struct mius_ipi_scp_to_host_message
-mius_ipi_scp_to_host_message_t;
+	struct mius_ipi_scp_to_host_message
+	mius_ipi_scp_to_host_message_t;
 static mius_ipi_scp_to_host_message_t usnd_ipi_receive;
 
 struct scp_mius_reserved_mem_t {
@@ -108,14 +108,14 @@ irqreturn_t iio_pollfunc_us(int irq, void *p)
 int32_t mius_debug_io_open(void)
 {
 
-	pr_info("[MIUS] %s()", __func__);
+	pr_debug("[MIUS] %s()", __func__);
 	if (debug_segment.reserved == 0) {
 		debug_segment.phys =
-		scp_get_reserve_mem_phys(SCP_ELLIPTIC_DEBUG_MEM);
+			scp_get_reserve_mem_phys(SCP_ELLIPTIC_DEBUG_MEM);
 		debug_segment.virt =
-		scp_get_reserve_mem_virt(SCP_ELLIPTIC_DEBUG_MEM);
+			scp_get_reserve_mem_virt(SCP_ELLIPTIC_DEBUG_MEM);
 		debug_segment.size =
-		scp_get_reserve_mem_size(SCP_ELLIPTIC_DEBUG_MEM);
+			scp_get_reserve_mem_size(SCP_ELLIPTIC_DEBUG_MEM);
 		debug_segment.reserved = 1;
 	}
 	return 1;
@@ -127,8 +127,8 @@ struct mius_dram_payload {
 	uint8_t data[MIUS_DEBUG_DATA_SIZE];
 };
 typedef
-struct mius_dram_payload
-mius_dram_payload_t;
+	struct mius_dram_payload
+	mius_dram_payload_t;
 
 static struct us_prox_data *g_us_prox;
 
@@ -149,7 +149,7 @@ struct us_prox_el_data {
 };
 
 #define US_SENSORS_CHANNELS(device_type, mask, index, mod, \
-ch2, s, endian, rbits, sbits, addr) \
+					ch2, s, endian, rbits, sbits, addr) \
 { \
 	.type = device_type, \
 	.modified = mod, \
@@ -168,9 +168,9 @@ ch2, s, endian, rbits, sbits, addr) \
 
 static const struct iio_chan_spec us_proximity_channels[] = {
 	US_SENSORS_CHANNELS(IIO_PROXIMITY,
-						BIT(IIO_CHAN_INFO_RAW) | BIT(IIO_CHAN_INFO_SAMP_FREQ),
-						0, 0, IIO_NO_MOD, 'u', IIO_LE, 16, 16, 0),
-						IIO_CHAN_SOFT_TIMESTAMP(2)
+		BIT(IIO_CHAN_INFO_RAW) | BIT(IIO_CHAN_INFO_SAMP_FREQ),
+		0, 0, IIO_NO_MOD, 'u', IIO_LE, 16, 16, 0),
+	IIO_CHAN_SOFT_TIMESTAMP(2)
 };
 
 static int us_buffer_postenable(struct iio_dev *indio_dev)
@@ -201,7 +201,7 @@ int us_afe_callback(int data)
 	struct timespec64 ts;
 	ktime_get_ts64(&ts);
 	el_data.timestamp = timespec64_to_ns(&ts);
-	pr_info("%s: data = %d\n", __func__, data);
+	pr_debug("%s: data = %d\n", __func__, data);
 
 	if (!data)
 		el_data.data1 = 0;
@@ -229,8 +229,8 @@ static mius_ipi_scp_to_host_message_header_t *get_header(
 		return NULL;
 	for (i = 0; i < MIUS_DRAM_PAYLOAD_MAX_OFFSET; ++i) {
 		mius_ipi_scp_to_host_message_header_t *header =
-		(mius_ipi_scp_to_host_message_header_t *)
-		(dram_payload + i);
+			(mius_ipi_scp_to_host_message_header_t *)
+			(dram_payload + i);
 		if (header->dram_payload_offset == header_id)
 			return header;
 	}
@@ -244,67 +244,67 @@ static int mius_data_io_ipi_handler(
 	__maybe_unused int32_t ret = -1;  // Seemed not be used
 
 	mius_dram_payload_t *dram_payload =
-	(mius_dram_payload_t *)debug_segment.virt;
+		(mius_dram_payload_t *)debug_segment.virt;
 
 	mius_ipi_scp_to_host_message_t *ipi_msg = data;
 	uint16_t target_ipi_message_count =
-	ipi_msg->header.dram_payload_offset;
+		ipi_msg->header.dram_payload_offset;
 
 	mius_ipi_scp_to_host_message_header_t *header;
 	void *payload = NULL;
 
-	pr_info("%s: start\n", __func__);
+	pr_debug("%s: start\n", __func__);
 
 	header = &ipi_msg->header;
 	payload = ipi_msg->data;
-	pr_info("MIUS Got data via ipi payload, addr: %p", payload);
+	pr_debug("MIUS Got data via ipi payload, addr: %p", payload);
 
 	if ((uint16_t) (target_ipi_message_count - current_ipi_counter) >
 		MIUS_DRAM_PAYLOAD_MAX_OFFSET) {
 		pr_warn("[MIUS] Offset mismatch, cur ipi counter:%u, target:%u!\n",
-				current_ipi_counter, target_ipi_message_count);
+			current_ipi_counter, target_ipi_message_count);
 		current_ipi_counter = target_ipi_message_count - MIUS_DRAM_PAYLOAD_MAX_OFFSET;
-		}
+	}
 
-		while (current_ipi_counter != target_ipi_message_count) {
-			++current_ipi_counter;
-			// check if payload is in dram buffer
-			header = get_header(dram_payload, current_ipi_counter);
-			if (header != NULL) {
-				payload = header + 1;
-			} else if (current_ipi_counter == target_ipi_message_count) {
-				// if not in dram buffer, it might be a small message in the ipi buffer
-				header = &ipi_msg->header;
-				payload = ipi_msg->data;
-			} else if (header == NULL) {
-				// message seems to be lost
-				pr_err("[MIUS] did not find payload with id %u",
-					   (unsigned int)current_ipi_counter);
-				continue;
-			}
-			switch (header->parameter_id) {
-				case ELLIPTIC_ULTRASOUND_PARAM_ID_ENGINE_DATA:
-					pr_info("MIUS engine data get %u",
-							 header->data_size);
-					if (((uint8_t *)payload)[23] == (uint8_t)66) {
-						pr_info("MIUS receive far signal");
-						us_afe_callback(0);
-					} else {
-						pr_info("MIUS receive near signal");
-						us_afe_callback(5);
-					}
-					break;
-				default:
-					pr_info("MIUS illegal param id: %u",
-							 header->parameter_id);
-					break;
-			}
+	while (current_ipi_counter != target_ipi_message_count) {
+		++current_ipi_counter;
+		// check if payload is in dram buffer
+		header = get_header(dram_payload, current_ipi_counter);
+		if (header != NULL) {
+			payload = header + 1;
+		} else if (current_ipi_counter == target_ipi_message_count) {
+			// if not in dram buffer, it might be a small message in the ipi buffer
+			header = &ipi_msg->header;
+			payload = ipi_msg->data;
+		} else if (header == NULL) {
+			// message seems to be lost
+			pr_err("[MIUS] did not find payload with id %u",
+			       (unsigned int)current_ipi_counter);
+			continue;
 		}
-		return 0;
+		switch (header->parameter_id) {
+			case ELLIPTIC_ULTRASOUND_PARAM_ID_ENGINE_DATA:
+				pr_debug("MIUS engine data get %u",
+						header->data_size);
+				if (((uint8_t *)payload)[23] == (uint8_t)66) {
+					pr_debug("MIUS receive far signal");
+					us_afe_callback(0);
+				} else {
+					pr_debug("MIUS receive near signal");
+					us_afe_callback(5);
+				}
+				break;
+			default:
+				pr_debug("MIUS illegal param id: %u",
+					header->parameter_id);
+				break;
+		}
+	}
+	return 0;
 }
 
 static ssize_t us_show_dump_output(struct device *dev,
-								   struct device_attribute *attr, char *buf)
+				struct device_attribute *attr, char *buf)
 {
 	struct us_prox_data *data;
 	unsigned long value;
@@ -319,13 +319,13 @@ static ssize_t us_show_dump_output(struct device *dev,
 }
 
 static ssize_t us_store_dump_output(struct device *dev,
-									struct device_attribute *attr, const char *buf, size_t size)
+		struct device_attribute *attr, const char *buf, size_t size)
 {
 	return 0;
 }
 
 static DEVICE_ATTR(dump_output, S_IWUSR | S_IRUGO,
-				   us_show_dump_output, us_store_dump_output);
+		us_show_dump_output, us_store_dump_output);
 
 
 static struct attribute *us_prox_attributes[] = {
@@ -343,52 +343,52 @@ static const struct iio_info us_proximity_info = {
 };
 
 int iio_trigger_buffer_setup(struct iio_dev *indio_dev,
-							 irqreturn_t (*h)(int irq, void *p),
-							 irqreturn_t (*thread)(int irq, void *p),
-							 const struct iio_buffer_setup_ops *setup_ops)
+        irqreturn_t (*h)(int irq, void *p),
+        irqreturn_t (*thread)(int irq, void *p),
+        const struct iio_buffer_setup_ops *setup_ops)
 {
-	struct iio_buffer *buffer;
-	int ret;
+    struct iio_buffer *buffer;
+    int ret;
 
-	buffer = iio_kefifo_allocate();
-	if (!buffer) {
-		ret = -ENOMEM;
-		goto error_ret;
-	}
+    buffer = iio_kefifo_allocate();
+    if (!buffer) {
+        ret = -ENOMEM;
+        goto error_ret;
+    }
 
-	iio_device_attach_buffer(indio_dev, buffer);
+    iio_device_attach_buffer(indio_dev, buffer);
 
-	indio_dev->pollfunc = iio_alloc_pollfunc(h,
-											 thread,
-										  IRQF_ONESHOT,
-										  indio_dev,
-										  "%s_consumer%d",
-										  indio_dev->name,
-										  indio_dev->id);
-	if (indio_dev->pollfunc == NULL) {
-		ret = -ENOMEM;
-		goto error_kfifo_free;
-	}
+    indio_dev->pollfunc = iio_alloc_pollfunc(h,
+            thread,
+            IRQF_ONESHOT,
+            indio_dev,
+            "%s_consumer%d",
+            indio_dev->name,
+            indio_dev->id);
+    if (indio_dev->pollfunc == NULL) {
+        ret = -ENOMEM;
+        goto error_kfifo_free;
+    }
 
-	/* Ring buffer functions - here trigger setup related */
-	/*
-	 i f (setup_ops)    *
-	 indio_dev->setup_ops = setup_ops;
-	 */
-	/*
-	 e lse              *
-	 indio_dev->setup_ops = &iio_trigger_buffer_setup_ops;
-	 */
+    /* Ring buffer functions - here trigger setup related */
+/*
+    if (setup_ops)
+        indio_dev->setup_ops = setup_ops;
+*/
+/*
+    else
+        indio_dev->setup_ops = &iio_trigger_buffer_setup_ops;
+*/
 
-	/* Flag that polled ring buffering is possible */
-	indio_dev->modes |= INDIO_BUFFER_SOFTWARE;
+    /* Flag that polled ring buffering is possible */
+    indio_dev->modes |= INDIO_BUFFER_SOFTWARE;
 
-	return 0;
+    return 0;
 
-	error_kfifo_free:
-	iio_kefifo_free(indio_dev->buffer);
-	error_ret:
-	return ret;
+error_kfifo_free:
+    iio_kefifo_free(indio_dev->buffer);
+error_ret:
+    return ret;
 }
 
 
@@ -398,12 +398,9 @@ static int us_proximity_iio_setup(struct device *pdev, struct us_prox_data *data
 	struct us_prox_data **priv_data;
 	int ret = 0;
 
-	dev_info(pdev, "%s: start\n", __func__);
-
 	idev = iio_device_alloc(pdev, sizeof(*priv_data));
 	if (!idev) {
 		pr_err("us prox IIO memory alloc fail\n");
-		dev_err(pdev, "us prox IIO memory alloc fail\n");
 		return -ENOMEM;
 	}
 
@@ -419,38 +416,30 @@ static int us_proximity_iio_setup(struct device *pdev, struct us_prox_data *data
 	priv_data = iio_priv(idev);
 	*priv_data = data;
 	ret = iio_trigger_buffer_setup(idev, iio_pollfunc_us, NULL,
-								   &us_buffer_setup_ops);
-	if (ret < 0) {
-		dev_err(pdev, "iio_trigger_buffer_setup failed: %d\n", ret);
+					&us_buffer_setup_ops);
+	if (ret < 0)
 		goto free_iio_p;
-	}
-
-	dev_info(pdev, "iio_trigger_buffer_setup success\n");
 
 	ret = iio_device_register(idev);
 	if (ret) {
 		pr_err("Proximity IIO register fail\n");
-		dev_err(pdev, "iio_device_register failed: %d\n", ret);
 		goto free_trigger_p;
 	}
-	dev_info(pdev, "iio_device_register success, device name: %s\n", idev->name);
-	dev_info(pdev, "%s: end (success)\n", __func__);
 	return ret;
 
-	free_trigger_p:
+free_trigger_p:
 	iio_trigger_buffer_cleanup(idev);
-	free_iio_p:
+free_iio_p:
 	data->prox_idev = NULL;
 	iio_device_free(idev);
-	dev_err(pdev, "%s: failed, ret=%d\n", __func__, ret);
 	return ret;
 }
 
 
 void iio_trigger_buffer_cleanup(struct iio_dev *indio_dev)
 {
-	iio_dealloc_pollfunc(indio_dev->pollfunc);
-	iio_kefifo_free(indio_dev->buffer);
+    iio_dealloc_pollfunc(indio_dev->pollfunc);
+    iio_kefifo_free(indio_dev->buffer);
 }
 
 static int us_proximity_teardown(struct us_prox_data *data)
@@ -467,8 +456,7 @@ static int us_prox_probe(struct platform_device *pdev)
 	int ret = 0;
 	struct us_prox_data *us_prox;
 
-	pr_info("%s: start\n", __func__);
-	dev_info(&pdev->dev, "%s: start\n", __func__);
+	pr_debug("%s: start\n", __func__);
 
 	us_prox = kzalloc(sizeof(*us_prox), GFP_KERNEL);
 	if (!us_prox)
@@ -481,28 +469,20 @@ static int us_prox_probe(struct platform_device *pdev)
 
 	mutex_init(&us_prox->mutex);
 	ret = us_proximity_iio_setup(&pdev->dev, us_prox);
-	if (ret < 0) {
-		dev_err(&pdev->dev, "us_proximity_iio_setup failed: %d\n", ret);
-		return ret;
-		}
 
-		{
-			int dbg_ret = mius_debug_io_open();
-			dev_info(&pdev->dev, "mius_debug_io_open returned: %d\n", dbg_ret);
-			if (dbg_ret < 0) {
-				dev_err(&pdev->dev, "mius_debug_io_open failed: %d\n", dbg_ret);
-				return dbg_ret;
-			}
-		}
+	ret = mius_debug_io_open();
+
+	if (ret < 0) {
+		pr_err("%s: iio setup failed ret = %d\n", __func__, ret);
+		return ret;
+	}
 	if (mtk_ipi_register(&scp_ipidev, IPI_IN_ELLIPTIC_ULTRA_0,
-		(mbox_pin_cb_t)mius_data_io_ipi_handler,
-						 NULL, &usnd_ipi_receive) != SCP_IPI_DONE) {
+			(mbox_pin_cb_t)mius_data_io_ipi_handler,
+			NULL, &usnd_ipi_receive) != SCP_IPI_DONE) {
 		pr_err("%s: scp registration failed ret = %d\n", __func__, ret);
-		dev_err(&pdev->dev, "scp registration failed\n");
-						 }
-						 pr_info("%s: end\n", __func__);
-						 dev_info(&pdev->dev, "%s: end\n", __func__);
-						 return ret;
+	}
+	pr_debug("%s: end\n", __func__);
+	return ret;
 }
 
 static int us_prox_remove(struct platform_device *pdev)
